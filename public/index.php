02e1561befce2d5cfe1dc9fa1aa09e66722911fb
veltophp <dev@veltophp.com>
2025-07-07 12:57:49 +0800

    update minor

--- public/index.php
+++ public/index.php
@@ -2,21 +2,27 @@
 
 define('VELTO_START', microtime(true));
 
+if (!defined('BASE_PATH')) {
+    define('BASE_PATH', dirname(__DIR__));
+}
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../vendor/veltophp/velto-core/src/helper/LoadHelpers.php';
+if (!defined('MODULES_PATH')) {
+    define('MODULES_PATH', BASE_PATH . '/modules');
+}
 
+if (!defined('CONFIG_PATH')) {
+    define('CONFIG_PATH', BASE_PATH . '/config');
+}
+
+require_once BASE_PATH . '/vendor/autoload.php';
+require_once BASE_PATH . '/vendor/veltophp/velto-core/src/helper/LoadHelpers.php';
 
 use Velto\Core\View\View;
 use Velto\Core\Env\Env;
 use Velto\Core\App\Kernel;
 use Velto\Core\Session\Session;
 
-Env::load(__DIR__ . '/../.env');
-
-define('BASE_PATH', dirname(__DIR__));
-define('MODULES_PATH', BASE_PATH . '/modules');
-define('CONFIG_PATH', BASE_PATH . '/config');
+Env::load(BASE_PATH . '/.env');
 
 Session::start();
 
